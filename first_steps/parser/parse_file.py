e00a0b3c635f3c7143630b0e70f307a561af24ee
khalid-sudo <laksantinikhalid12@gmail.com>
2026-01-05 23:29:57 +0100

    first version of recursive funtion

--- first_steps/parser/parse_file.py
+++ first_steps/parser/parse_file.py
@@ -13,26 +13,28 @@ def read_parsed(value):
                 if isinstance(val,(ast.AST,list)):
                     read_parsed(val)
                 else:
-                    print(f"$$which has {key} with value =>{val}") 
+                    if key  not in ["lineno","end_lineno","col_offset","end_col_offset"]:
+                        print(f"which has {key} with value =>{val}") 
         elif isinstance(value,list) and len(value)>0  and isinstance(value[0], ast.AST):
-             for key,val in value[0].__dict__.items():
-                if isinstance(val,(ast.AST,list)):
-                    read_parsed(val)
-                else:
-                    print(f"**which has {key} with value => {val}") 
-        else:
-            return f"value => {value}"  
+            for index in range(len(value)):
+                print(f"the value Type is {value[index]}")
+                for key,val in value[index].__dict__.items():
+                    if isinstance(val,(ast.AST,list)):
+                        read_parsed(val)
+                    else:
+                        if key  not in ["lineno","end_lineno","col_offset","end_col_offset"]:
+                           print(f"which has {key} with value => {val}") 
+        # else:
+        #     return f"value => {value}"  
 
 def parse_file(file_name: str):
     try:
         source_code = read_file(Path(file_name))
         tree = ast.parse(source_code,filename=file_name,mode="exec",type_comments=True,feature_version=(version_info.major, version_info.minor))
-        #print(ast.dump(tree))
+        print(ast.dump(tree))
         tree = ast.parse(source_code)
-        #assignment_node = tree.body
         for node in tree.body:
             for field_name, value in ast.iter_fields(node):
-                #print(f"the field name is {field_name} and it's value is {read_parsed(value)}")
                 print(f"the field name is {field_name} and it's value is {value}")
                 print(read_parsed(value))
     except Exception as e:
