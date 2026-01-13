92aef0b730f73c71139c0ea46ad72ccd816db49f
khalid-sudo <laksantinikhalid12@gmail.com>
2026-01-13 23:08:29 +0100

    added more example where  set is faster than list

--- first_steps/data_structures/Hash_sets/hashset.py
+++ first_steps/data_structures/Hash_sets/hashset.py
@@ -19,4 +19,36 @@ def compare_set_to_list():
             print(f"the {_+1}'s test to check if {value_to_check} is in dataset => {value_to_check in data_list}")
     print(f"for 100000 iteration in a list it took {time.time() - start_time}'s") #output for 100000  iteration in a list it took 585.9723460674286's
 
-compare_set_to_list()
+#compare_set_to_list()
+
+"""
+Problems solved with using list and set. Pinpoint the elements that appear in both of the given lists.
+"""
+#Problem 1: Array Intersection
+#Naive approache
+list1 = [3,5,10,54,3,45,33,21,33,24]
+list2 = [4,8,3,76,987,65,3,5]
+intersection_list = []
+start_time = time.time()
+for num1 in list1:
+    if num1 not in intersection_list:
+        for num2 in list2:
+            if num1==num2:
+                    intersection_list.append(num1)
+                    list2.remove(num1)
+                    break
+    else:
+        continue
+print(f"it took {time.time() - start_time}'s") #output: it took 5.9604644775390625e-06's
+print(intersection_list)
+
+list1 = [3,5,10,54,3,45,33,21,33,24]
+list2 = [4,8,3,76,987,65,3,5]
+#now using set
+start_time = time.time()
+set1 = set(list1) 
+set2 = set(list2)
+intersection = set1 & set2
+
+print(intersection)
+print(f"it took {time.time() - start_time}'s") #output: it took 4.0531158447265625e-06's
