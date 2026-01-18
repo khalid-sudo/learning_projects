17e3a9b348759645209dfe7f2deab7cda776eebe
khalid-sudo <laksantinikhalid12@gmail.com>
2026-01-18 11:52:35 +0100

    optimized fibonachi

--- first_steps/data/test1.py
+++ first_steps/data/test1.py
@@ -4,3 +4,12 @@ def fibonaci(n):
     else:
         return fibonaci(n-1) + fibonaci(n-2)
 print(fibonaci(40))
+
+#optimized without cache
+def fib(n, computed={0: 0, 1: 1}):
+
+    if n not in computed:
+
+        computed[n] = fib(n - 1, computed) + fib(n - 2, computed)
+
+    return computed[n]
